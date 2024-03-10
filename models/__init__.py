ae98e1b8fcdcc1021e9c408b267789a842eaf370
djshampz904 <martin.kimani@strathmore.edu>
2024-03-10 19:30:40 +0300

    pyconsole

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,11 @@
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
 from .user import User
+from .state import State
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .review import Review
 
 
 storage = FileStorage()
@@ -8,5 +13,10 @@ storage.reload()
 
 CLASSES = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
 }
